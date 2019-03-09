3cfa08efd0fd997dc91cb6e86e9e0097eebf2840
Piotr Baran <bp84dev@outlook.com>
2019-03-09 23:24:40 +0000

    Added support for loading images from mongo db

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,8 +1,11 @@
 'use strict';
+// import .env
+require('dotenv').config();
 // import required modules
 const User = require('../models/user');
 const Region = require('../models/region');
 const Island = require('../models/island');
+const Image = require('../models/image');
 
 const Dashboard = {
   // load user dashboard page, enable authentication
@@ -11,7 +14,9 @@ const Dashboard = {
       try {
         const id = request.auth.credentials.id;
         const user = await User.findById(id);
-        const islands = await Island.find({}).populate('costalZone');
+        const islands = await Island.find({})
+          .populate('costalZone')
+          .populate('images', 'uuid'); //populate uuid for images only
         // convert to plain js object and enhance result to add 'category' field required by semantic ui search control
         const jsIslands = islands.map(function(model) {
           return model.toObject();
@@ -20,9 +25,14 @@ const Dashboard = {
         jsIslands.forEach(island => {
           island.category = island.costalZone.name;
         });
-        // convert enhanced collection into string json to be semantic ui search control
+        // convert enhanced collection into string json to be used by semantic ui search control
         const jsonIslands = JSON.stringify(jsIslands);
-        return h.view('dashboard', { title: 'Islands of Ireland', user: user, jsonIslands: jsonIslands });
+        return h.view('dashboard', {
+          title: 'Islands of Ireland',
+          user: user,
+          jsonIslands: jsonIslands,
+          apiKey: process.env.google_api_key
+        });
       } catch (err) {
         return h.view('login', { errors: [{ message: err.message }] });
       }
